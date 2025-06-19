123ffdc4bc5548c1f0b6bb513740e376cb126256
rs-quyhp <quyhp@runsystem.net>
2025-06-19 21:22:43 +0700

    23. design component input practice

---
+++ src/todo/inputTodo.component.tsx
@@ -0,0 +1,14 @@
+const InputToDo = () => {
+  return (
+    <div>
+      <span>Add new todo</span>
+      <div>
+        <input type="text" />
+        &nbsp; &nbsp;
+        <button>Save</button>
+      </div>
+    </div>
+  );
+};
+
+export default InputToDo;
