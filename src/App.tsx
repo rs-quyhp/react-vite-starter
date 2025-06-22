1f876e09413afee6d8f5defeeddfa6d84a054284
rs-quyhp <quyhp@runsystem.net>
2025-06-22 20:44:06 +0700

    28-29. render lists, key

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import "./App.css";
 import InputToDo from "./todo/inputTodo.component";
 
 function App() {
+  const todoList = ["Todo 1", "Todo 2", "Todo 3", "Todo 4", "Todo 5"];
+
   return (
     <>
-      <InputToDo 
-        name={'quyhp'}
+      <InputToDo
+        name={"quyhp"}
         age={25}
         info={{
-          gender: 'male',
-          address: 'DN'
+          gender: "male",
+          address: "DN",
         }}
       />
+
+      <ul>
+        {todoList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </>
   );
 }
