f455fd82f9e63e774577b6e6d1bd8dff10f40c2a
rs-quyhp <quyhp@runsystem.net>
2025-06-24 21:01:33 +0700

    50. fetch api using react

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,6 +1,42 @@
+import { useEffect } from "react";
 import "../../styles/users.css";
 
 const UsersTable = () => {
+  const getData = async () => {
+    // Get access token
+    const res = await fetch("http://localhost:8000/api/v1/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: "admin@gmail.com",
+        password: "123456",
+      }),
+    });
+
+    const data = await res.json();
+
+    const accessToken = data?.data?.access_token;
+
+    // Fetch list user
+    const usersRes = await fetch("http://localhost:8000/api/v1/users/all", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    const usersData = await usersRes.json();
+
+    console.log("usersData", usersData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <h2>HTML Table</h2>
