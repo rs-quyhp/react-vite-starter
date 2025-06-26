1e998a3abfc4bce38b2a2a5f8c141e4c77bd4a4d
rs-quyhp <quyhp@runsystem.net>
2025-06-26 21:17:41 +0700

    57-58. install antd & design admin layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 import UserPage from "./screens/users.page.tsx";
+import { HomeOutlined, UserOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const items: MenuItem[] = [
+  {
+    label: <Link to="/">Home</Link>,
+    key: "home",
+    icon: <HomeOutlined />,
+  },
+  {
+    label: <Link to="/users">User Management</Link>,
+    key: "users",
+    icon: <UserOutlined />,
+  },
+];
+
+const Header: React.FC = () => {
+  const [current, setCurrent] = useState("home");
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
+  );
+};
+
+const LayoutAdmin = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/users",
-    element: <UserPage />,
+    element: <LayoutAdmin />,
+    children: [
+      { index: true, element: <App /> },
+      {
+        path: "users",
+        element: <UserPage />,
+      },
+    ],
   },
 ]);
 
