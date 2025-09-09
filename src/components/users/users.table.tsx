393fb089fa62c779e9ef8550445a9b70e1bb79a9
quyhp <quyhp@runsystem.net>
2025-09-09 14:46:18 +0700

    63-66. add user modal

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,7 +1,17 @@
 import { useEffect, useState } from 'react';
 // import '../../styles/users.css';
-import { Table } from 'antd';
+import {
+  Button,
+  Dropdown,
+  Input,
+  message,
+  Modal,
+  notification,
+  Space,
+  Table,
+} from 'antd';
 import { ColumnsType } from 'antd/es/table';
+import { DownOutlined, PlusOutlined } from '@ant-design/icons';
 
 interface IUser {
   email: string;
@@ -12,6 +22,79 @@ interface IUser {
 
 const UsersTable = () => {
   const [listUsers, setListUsers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('male');
+  const [address, setAddress] = useState('');
+  const [role, setRole] = useState('ADMIN');
+
+  const handleOk = async () => {
+    const data = {
+      name,
+      email,
+      password,
+      age,
+      gender,
+      address,
+      role,
+    };
+
+    // Get access token
+    const res = await fetch('http://localhost:8000/api/v1/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: 'admin@gmail.com',
+        password: '123456',
+      }),
+    });
+
+    const authData = await res.json();
+
+    const accessToken = authData?.data?.access_token;
+
+    // Create user
+    const response = await fetch('http://localhost:8000/api/v1/users', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    const responseData = await response.json();
+
+    if (responseData.data) {
+      getData();
+      notification.success({
+        message: 'User created successfully',
+      });
+      handleCloseModal();
+    } else {
+      notification.error({
+        message: 'Error',
+        description: JSON.stringify(responseData.message),
+      });
+    }
+  };
+
+  const handleCloseModal = () => {
+    setName('');
+    setEmail('');
+    setPassword('');
+    setAge('');
+    setGender('male');
+    setAddress('');
+    setRole('ADMIN');
+    setIsModalOpen(false);
+  };
 
   const getData = async () => {
     // Get access token
@@ -67,7 +150,108 @@ const UsersTable = () => {
   ];
   return (
     <div>
-      <h2>Users Table</h2>
+      <Modal
+        title="Add new user"
+        closable={true}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCloseModal}
+        maskClosable={false}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+          <div>
+            <label>Name:</label>
+            <Input value={name} onChange={(e) => setName(e.target.value)} />
+          </div>
+          <div>
+            <label>Email:</label>
+            <Input value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div>
+            <label>Password:</label>
+            <Input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+            />
+          </div>
+          <div>
+            <label>Age:</label>
+            <Input
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+              type="number"
+            />
+          </div>
+          <div style={{ display: 'flex', gap: 10, alignItems: 'center' }}>
+            <label>Gender:</label>
+            <Dropdown
+              menu={{
+                items: [
+                  { label: 'Male', key: 'male' },
+                  { label: 'Female', key: 'female' },
+                ],
+                onClick: ({ key }) => {
+                  setGender(key);
+                },
+                defaultValue: gender,
+              }}
+            >
+              <Button>
+                <Space>
+                  {gender.charAt(0).toUpperCase() + gender.slice(1)}
+                  <DownOutlined />
+                </Space>
+              </Button>
+            </Dropdown>
+          </div>
+          <div>
+            <label>Address:</label>
+            <Input
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+          <div style={{ display: 'flex', gap: 10, alignItems: 'center' }}>
+            <label>Role:</label>
+            <Dropdown
+              menu={{
+                items: [
+                  { label: 'ADMIN', key: 'ADMIN' },
+                  { label: 'USER', key: 'USER' },
+                ],
+                onClick: ({ key }) => {
+                  setRole(key);
+                },
+                defaultValue: role,
+              }}
+            >
+              <Button>
+                <Space>
+                  {role.charAt(0).toUpperCase() + role.slice(1)}
+                  <DownOutlined />
+                </Space>
+              </Button>
+            </Dropdown>
+          </div>
+        </div>
+      </Modal>
+      <div
+        style={{
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          display: 'flex',
+        }}
+      >
+        <h2>Users Table</h2>
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          onClick={() => setIsModalOpen(true)}
+        >
+          New user
+        </Button>
+      </div>
       <Table columns={columns} dataSource={listUsers} rowKey={'_id'} />
       {/* <table>
         <tr>
